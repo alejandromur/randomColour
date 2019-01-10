9097e005447e382226934e5161d451d3cdc73d78
Alejandro Mur <alejandro.mur@boream.com>
2019-01-10 13:48:23 +0100

    arrays letters and numbers merged

--- js/random-colour.js
+++ js/random-colour.js
@@ -7,50 +7,33 @@
 	RGB (pattern) => R: xx G: xx B: xx
 */
 
-const letters = ['A', 'B', 'C', 'D', 'E', 'F'];
-const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const ITEMS = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 const root = document.querySelector('body');
-const title = document.querySelector('.module__text');		
+const title = document.querySelector('.module__text');
 
 function getRandomNumber(n = 2) {
 	return 1 + (Math.floor(Math.random() * n));
 }
 
-function isOdd(n) {
-	if (n % 2 === 0) {
-		return 'letters';
-	} else {
-		return 'numbers';
-	}	
-}
+function getRandomItem() {
+	let itemPosition = 0;	
+	itemPosition = getRandomNumber(ITEMS.length - 1);
 
-function getRandomItem(str) {
-	let itemPosition = 0;
-	let item;
-	if ( str === 'letters' ) {
-		itemPosition = getRandomNumber(letters.length - 1);
-		item = letters[itemPosition];
-	} else {
-		itemPosition = getRandomNumber(numbers.length - 1);
-		item = numbers[itemPosition];
-	}
-	return item;
+	return ITEMS[itemPosition];
 }
 
 class RandomColour {
 
 	generateRandomColour() {
-		this.colour = '#';
+		let colour = '#';
 
-		for(let i = 0; i < 6; i++) {
-			let randomNumber = getRandomNumber(2);
-			let letterOrNumber = isOdd(randomNumber);
-			let randomItem = getRandomItem(letterOrNumber);
-			this.colour += randomItem;
+		for (let i = 0; i < 6; i++) {
+			let randomItem = getRandomItem();
+			colour += randomItem;
 		}
 
-		return this.colour;
+		return colour;
 	}
 
 	applyRandomColour(randomColour) {
