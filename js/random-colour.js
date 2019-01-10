1412ed35627cc4115be0c040ede3238473fdda85
Alejandro Mur <alejandro.mur@boream.com>
2019-01-10 13:39:12 +0100

    ES6, CSS vars, HTML5

--- js/random-colour.js
+++ js/random-colour.js
@@ -1,68 +1,67 @@
 /*
 	@alejandro_mur
-	http://www.mamutlove.es
-	v1.1.0
-	2016/10/16
+	https://www.mamutlove.com
+	v2.0.0
+	2019/01/10
 	HEXADECIMAL COLOUR
 	RGB (pattern) => R: xx G: xx B: xx
 */
 
-var getRandomColor = {
-	letters : ["A","B","C","D","E","F"],
-	numbers : ["0","1","2","3","4","5","6","7","8","9"],
-	body : document.querySelector("body"),
-	colourName : document.querySelector(".module__title"),
-	randomColor : "#",
-	init : function () {
-		this.randomColor = "#";
-		this.selectColour(this.randomColor);
-	},
-	selectColour : function () {
-		for(var i = 0; i < 6; i++) {			
-			var num = this.getRandomNumber(2);
-			var characterChoosen = this.evenOrOdd(num);
-			var position = this.getRandomNumber(characterChoosen);
-			this.getCharacterFromArray(characterChoosen,position);
-		}
-	},
-	getRandomNumber : function (n){
-		if(n == "letters"){
-			n = this.letters.length;
-		}else if(n == "numbers"){
-			n = this.numbers.length;
-		}
-		var num = 1+(Math.floor(Math.random()*n));
-		return num;
-	},
-	evenOrOdd : function (num) {
-		if(num % 2 == 0){
-			return "letters";
-		}else{
-			return "numbers";
-		}
-	},
-	getCharacterFromArray : function (markArr,pos) {
-		var arr = [];
-		if(markArr == "letters"){
-			arr = this.letters;
-		}else if(markArr == "numbers"){
-			arr = this.numbers;
-		}
-		this.randomColor += arr[pos-1];
-		this.applyColour(this.randomColor);
-	},
-	applyColour : function (colour) {
-		this.colourName.innerHTML = colour;
-		this.body.style.backgroundColor = colour;
-	}
+const letters = ['A', 'B', 'C', 'D', 'E', 'F'];
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+const root = document.querySelector('body');
+const title = document.querySelector('.module__text');		
+
+function getRandomNumber(n = 2) {
+	return 1 + (Math.floor(Math.random() * n));
 }
 
-var buttonGetRandomColor = document.querySelector(".js-getcolour");
+function isOdd(n) {
+	if (n % 2 === 0) {
+		return 'letters';
+	} else {
+		return 'numbers';
+	}	
+}
 
-// LAUNCH OBJ
-getRandomColor.init();
+function getRandomItem(str) {
+	let itemPosition = 0;
+	let item;
+	if ( str === 'letters' ) {
+		itemPosition = getRandomNumber(letters.length - 1);
+		item = letters[itemPosition];
+	} else {
+		itemPosition = getRandomNumber(numbers.length - 1);
+		item = numbers[itemPosition];
+	}
+	return item;
+}
 
-// CHANGE COLOUR ON CLICK
-buttonGetRandomColor.addEventListener("click", function () {
-	getRandomColor.init();
-}, false);
+class RandomColour {
+
+	generateRandomColour() {
+		this.colour = '#';
+
+		for(let i = 0; i < 6; i++) {
+			let randomNumber = getRandomNumber(2);
+			let letterOrNumber = isOdd(randomNumber);
+			let randomItem = getRandomItem(letterOrNumber);
+			this.colour += randomItem;
+		}
+
+		return this.colour;
+	}
+
+	applyRandomColour(randomColour) {
+		root.style.setProperty('--colour-generated', randomColour);
+		title.innerHTML = randomColour;
+	}
+};
+
+const rc = new RandomColour();
+
+const button = document.querySelector('.js-getcolour');
+button.addEventListener('click', () => {
+	rc.applyRandomColour(rc.generateRandomColour());
+});
